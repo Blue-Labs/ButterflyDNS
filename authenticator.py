b3e826456d1b9ce8f14b54f09e50bcf530a7a8af
David Ford <david@blue-labs.org>
2017-07-30 22:35:24 -0400

    bring source up to par with production

--- authenticator.py
+++ authenticator.py
@@ -5,10 +5,10 @@ will instantiate the below AuthenticatorSession() class, you do not need to run 
 yourself.
 """
 
-__version__  = '1.2.1'
+__version__  = '1.2.2'
 __author__   = 'David Ford <david@blue-labs.org>'
 __email__    = 'david@blue-labs.org'
-__date__     = '2016-Apr-21 00:51Z'
+__date__     = '2017-Jul-31 02:12Z'
 __license__  = 'Apache 2.0'
 
 
@@ -18,22 +18,31 @@ import base64
 import hashlib
 import datetime
 import configparser
+
 from ldap3 import Server
 from ldap3 import Connection
 from ldap3 import Tls
 from ldap3 import ALL_ATTRIBUTES
+from ldap3 import ALL
+from ldap3 import SUBTREE
+from ldap3 import LEVEL
+from ldap3 import MODIFY_REPLACE
 from ldap3 import AUTH_SIMPLE
 from ldap3 import LDAPInvalidCredentialsResult
 from ldap3 import LDAPSizeLimitExceededResult
 from ldap3 import LDAPException
-from ldap3.core.exceptions import LDAPSessionTerminatedByServer
+
+from ldap3.core.exceptions import LDAPInvalidCredentialsResult
+from ldap3.core.exceptions import LDAPSizeLimitExceededResult
+from ldap3.core.exceptions import LDAPException
+from ldap3.core.exceptions import LDAPSessionTerminatedByServerError
 from ldap3.core.exceptions import LDAPSocketReceiveError
 
-from pprint import pprint
-from base64 import urlsafe_b64decode as dcode
-from dateutil import parser as dateparser
-from twisted.internet.defer import inlineCallbacks
-from autobahn.twisted.wamp import ApplicationSession
+from pprint                  import pprint
+from base64                  import urlsafe_b64decode as dcode
+from dateutil                import parser as dateparser
+from twisted.internet.defer  import inlineCallbacks
+from autobahn.twisted.wamp   import ApplicationSession
 from autobahn.wamp.exception import ApplicationError
 
 
@@ -68,11 +77,19 @@ class LDAP():
         while deadtime > datetime.datetime.utcnow():
             try:
                 ca_file = '/etc/ssl/certs/ca-certificates.crt'
-                tlso    = Tls(ca_certs_file=ca_file, validate=ssl.CERT_REQUIRED,
+                tlso    = Tls(ca_certs_file=ca_file,
+                              validate=ssl.CERT_REQUIRED,
                               valid_names=self.valid_names)
-                server  = Server(self.host, port=self.port, use_ssl=False, tls=tlso)
-                ctx     = Connection(server, user=self.userdn, password=self.passwd,
-                                  raise_exceptions=True, authentication=AUTH_SIMPLE)
+                server  = Server(host=self.host,
+                                 port=self.port,
+                                 use_ssl=False,
+                                 get_info=ALL,
+                                 tls=tlso)
+                ctx     = Connection(server,
+                                  user=self.userdn,
+                                  password=self.passwd,
+                                  raise_exceptions=True,
+                                  authentication=SIMPLE)
                 ctx.open()
                 ctx.start_tls()
                 if not ctx.bind():
@@ -80,8 +97,9 @@ class LDAP():
                     raise Exception('Failed to bind')
                 break
 
-            except (LDAPSessionTerminatedByServer, LDAPSocketReceiveError):
-                time.sleep(1)
+            except (LDAPSessionTerminatedByServerError, LDAPSocketReceiveError) as e:
+              print('LDAP server connect error: {}'.format(e))
+              time.sleep(1)
 
             except Exception as e:
                 print('LDAP error: {}'.format(e))
@@ -126,7 +144,7 @@ class AuthenticatorSession(ApplicationSession):
       self._ldap = LDAP(cfg)
 
       def checkPassword(challenge_password, password):
-         challenge_bytes = dcode(challenge_password[6:])
+         challenge_bytes = dcode(challenge_password)[6:]
          digest,salt     = challenge_bytes[:20],challenge_bytes[20:]
          hr = hashlib.sha1(password.encode())
          hr.update(salt)
@@ -150,6 +168,10 @@ class AuthenticatorSession(ApplicationSession):
               .format(ticket, authid))
 
          principal = self._ldap.ctx.response[0]['attributes']
+
+         if not isinstance(principal['realm'], list):
+            principal['realm'] = [principal['realm']]
+
          if not 'roleAdmin' in principal:
             principal['roleAdmin'] = [False]
          if not 'jpegPhoto' in principal:
@@ -158,9 +180,16 @@ class AuthenticatorSession(ApplicationSession):
             principal['displayName'] = [authid]
          if not 'department' in principal:
             principal['department'] = ['bit mover']
+         if not isinstance(principal['department'], list):
+            principal['department'] = [principal['department']]
+         if not len(principal['department']):
+            principal['department'] = ['bit mover']
 
-         if principal['jpegPhoto'][0]:
-            principal['jpegPhoto'] = [base64.b64encode(principal['jpegPhoto'][0])]
+         if 'jpegPhoto' in principal and principal['jpegPhoto']:
+                if isinstance(principal['jpegPhoto'], list):
+                    principal['jpegPhoto'] = [base64.b64encode(p) for p in principal['jpegPhoto']]
+                else:
+                    principal['jpegPhoto'] = base64.b64encode(principal['jpegPhoto'])
 
          if not 'notBefore' in principal and 'notAfter' in principal:
             raise ApplicationError(u'org.head.butterflydns.invalid_role_configured',
@@ -180,21 +209,21 @@ class AuthenticatorSession(ApplicationSession):
             'realm': principal['realm'][0],
             'role':  principal['role'][0],
             'extra': {
-               'roleAdmin': principal['roleAdmin'][0],
-               'jpegPhoto': principal['jpegPhoto'][0],
-               'department': principal['department'][0],
-               'displayName': principal['displayName'][0]
+               'roleAdmin': principal['roleAdmin'],
+               'jpegPhoto': principal['jpegPhoto'],
+               'department': principal['department'],
+               'displayName': principal['displayName']
             }
          }
 
          resp = {
-            'realm': principal['realm'][0],
-            'role':  principal['role'][0],
+            'realm': principal['realm'],
+            'role':  principal['role'],
             'extra': {
-               'roleAdmin': principal['roleAdmin'][0],
+               'roleAdmin': principal['roleAdmin'],
                'jpegPhoto': '<suppressed>',
-               'department': principal['department'][0],
-               'displayName': principal['displayName'][0]
+               'department': principal['department'],
+               'displayName': principal['displayName']
             }
          }
 
